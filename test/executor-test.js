df7f11476519af66ed4748adc3e47dcf4d2e4d3a
nishant <nishant.singh87@gmail.com>
2016-05-20 14:54:43 +0530

    readonly scope params in expressions.

--- test/executor-test.js
+++ test/executor-test.js
@@ -2,10 +2,23 @@ var expect  = require('chai').expect,
     execute  = require("../src/executer");
 
 describe('Execute JS', function() {
-  it('should inject fields with primary values to context', function (done) {
+  it('should inject fields with primary values to context', function () {
     var scope      = {id: 1},
         expression = "id + 99";
     expect(execute.execute(scope, expression)).to.equal(100);
-    done();
   });
+
+  it('should allow operation on context values', function () {
+    var scope      = {id: 1, name: "Vijay"},
+        expression = '(id + 99) + "-" + name';
+    expect(execute.execute(scope, expression)).to.equal("100-Vijay");
+  });
+
+  it('should not update context values', function () {
+    var scope      = {id: 1, name: "Vijay"},
+        expression = 'id = 2';
+    expect(execute.execute(scope, expression)).to.equal(2);
+    expect(scope.id).to.equal(1);
+  });
+
 });
