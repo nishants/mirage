0ea31732b7d6bf756cdd3298018a64ecc8b82022
nishant <nishant.singh87@gmail.com>
2016-05-20 15:27:58 +0530

    refactoring test.

--- test/scope-test.js
+++ test/scope-test.js
@@ -6,7 +6,6 @@ describe('Execute JS', function() {
   it('should inject fields with primary values to context', function () {
     var scope       = scopes.create({id: 1}),
         expression  = "id + 99";
-
     expect(scope.execute(expression)).to.equal(100);
   });
 
@@ -20,19 +19,28 @@ describe('Execute JS', function() {
     var injected    = {id: 1, name: "Vijay"},
         scope       = scopes.create(injected),
         expression  = 'id = 2';
-
     expect(scope.execute(expression)).to.equal(2);
     expect(injected.id).to.equal(1);
   });
 
   it('should inject object fields', function () {
-    var scope       = scopes.create({data: {id: 1, address: {street: "my-home"}}}),
+    var scope       = scopes.create({
+          data: {id: 1,
+            address: {street: "my-home"}
+          }
+        }),
         expression  = 'data.id + "-" + data.address.street';
+
     expect(scope.execute(expression)).to.equal("1-my-home");
   });
 
   it('should allow calling function on injectables', function () {
-    var scope       = scopes.create({callMe: function(){return "called-me";}, child: {callMeToo: function(){return "called-me-too";}}}),
+    var scope       = scopes.create({
+          callMe: function(){return "called-me";},
+          child: {callMeToo: function(){
+            return "called-me-too";}
+          }
+        }),
         expression  = 'callMe() + "-" + child.callMeToo()';
 
     expect(scope.execute(expression)).to.equal("called-me-called-me-too");
