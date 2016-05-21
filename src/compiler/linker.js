6de6abedc6da924c237ffb48277d5fa01d86d01a
nishant <nishant.singh87@gmail.com>
2016-05-21 18:49:12 +0530

    extract directives interpolator from linker.

--- src/compiler/linker.js
+++ src/compiler/linker.js
@@ -1,3 +1,16 @@
+function directivesOn(element) {
+  var directives =[];
+  for(var child in element){
+    child.startsWith("@") ? directives.push(element[child]) && (delete element[child]) :"";
+  }
+  return {
+    list: directives,
+    link: function(scope, element){
+      return require("./compiler").compile(scope, element);
+    }
+  };
+};
+
 module.exports = {
   link: link = function(scope, element){
     var isString        = (typeof element == "string"),
@@ -5,8 +18,10 @@ module.exports = {
         hasExpressions  = isString ? element.indexOf("{{") != -1 : false,
         expression      = hasExpressions ? element.split("{{")[1].split("}}")[0] : null,
         expressionValue = expression ? scope.execute(expression) : null,
+        directives      = directivesOn(element),
+        hasDirectives   = directives.list.length > 0,
         parsed          = expressionValue ? element.replace("{{"+expression+"}}", expressionValue) : element;
 
-    return parsed = isSubtree ? require("./compiler").compile(scope, element) : parsed;
+    return parsed = isSubtree ? directives.link(scope, element) : parsed;
   }
 };
