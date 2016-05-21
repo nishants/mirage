bff4c107284edcd3b9476b71d1060f486f5e20dd
nishant <nishant.singh87@gmail.com>
2016-05-21 13:40:27 +0530

    refactoring send-file.js

--- src/send-file.js
+++ src/send-file.js
@@ -2,18 +2,12 @@ var fs = require("fs"),
     scopes  = require("./scope"),
     compiler  = require("./compiler/compiler");
 
-var readFile = function (path) {
-      return JSON.parse(fs.readFileSync(path));
-    },
-    parseRequest = function(req){
-      return {body: req.body, path: req.params, query: req.query};
-    },
-    sendFile = function (path) {
+var sendFile = function (path) {
       return {
         path: path,
         send: function (req, res) {
-          var scope       = scopes.create({request: parseRequest(req)}),
-              template    = readFile(this.path),
+          var scope       = scopes.create({request: {body: req.body, path: req.params, query: req.query}}),
+              template    = JSON.parse(fs.readFileSync(this.path)),
               parsed      = compiler.compile(scope, template),
               responseBody= parsed.body;
 
