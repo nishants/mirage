e9066151d1a583129c0a66a5dd8edb2f52d20a7b
nishant <nishant.singh87@gmail.com>
2016-05-21 12:05:51 +0530

    relative path for demo.js

--- demo.js
+++ demo.js
@@ -1,8 +1,8 @@
 var mirage = require("./src/mirage").create();
 
-mirage.post("/user").sendFile("../sample/create.json");
-mirage.get("/user/:id").sendFile("../sample/request-path-param.json");
-mirage.get("/user").sendFile("../sample/request-url-param.json");
+mirage.post("/user").sendFile("./sample/create.json");
+mirage.get("/user/:id").sendFile("./sample/request-path-param.json");
+mirage.get("/user").sendFile("./sample/request-url-param.json");
 
 mirage.app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
