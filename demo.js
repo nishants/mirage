722a0022c3eb164f1368e4dcbf7b7ade7cf36ea6
nishant <nishant.singh87@gmail.com>
2016-05-21 20:16:41 +0530

    updating demo.js

--- demo.js
+++ demo.js
@@ -1,8 +1,8 @@
 var mirage = require("./src/mirage").create();
 
-mirage.post("/user").sendFile("../sample/create.json");
-mirage.get("/user/:id").sendFile("../sample/request-path-param.json");
-mirage.get("/user").sendFile("../sample/request-url-param.json");
+mirage.post("/create").sendFile("../sample/create.json");
+mirage.get("/get/:id").sendFile("../sample/request-path-param.json");
+mirage.get("/params").sendFile("../sample/request-url-param.json");
 mirage.get("/hello").sendFile("../sample/hello.json");
 mirage.post("/repeater").sendFile("../sample/repeater.json");
 
