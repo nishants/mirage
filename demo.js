031ad7965006197777ed52f5534b09dbac815191
nishant <nishant.singh87@gmail.com>
2016-05-21 11:49:39 +0530

    moving demo.js

--- demo.js
+++ demo.js
@@ -1,4 +1,5 @@
-var mirage     = require("./mirage").create();
+var mirage = require("./src/mirage").create();
+
 mirage.post("/user").sendFile("../sample/create.json");
 mirage.get("/user/:id").sendFile("../sample/request-path-param.json");
 mirage.get("/user").sendFile("../sample/request-url-param.json");
