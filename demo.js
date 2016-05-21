c7428bec7b3d1b9ce97a0e17ca0242bf2d700157
nishant <nishant.singh87@gmail.com>
2016-05-21 15:36:40 +0530

    demo.js

--- demo.js
+++ demo.js
@@ -1,8 +1,9 @@
 var mirage = require("./src/mirage").create();
 
-mirage.post("/user").sendFile("./sample/create.json");
-mirage.get("/user/:id").sendFile("./sample/request-path-param.json");
-mirage.get("/user").sendFile("./sample/request-url-param.json");
+mirage.post("/user").sendFile("../sample/create.json");
+mirage.get("/user/:id").sendFile("../sample/request-path-param.json");
+mirage.get("/user").sendFile("../sample/request-url-param.json");
+mirage.get("/hello").sendFile("../sample/hello.json");
 
 mirage.app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
