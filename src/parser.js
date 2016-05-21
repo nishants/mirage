8decdba5bd62527bd9ce006547ef6678af759db0
nishant <nishant.singh87@gmail.com>
2016-05-21 11:15:42 +0530

    refactoring parser: extract link function.

--- src/parser.js
+++ src/parser.js
@@ -1,20 +1,20 @@
-var parse = function (scope, template) {
-  var result = {};
-  // find and replace expressions with there resulting values.
-  for (var node in template) {
-    var nodeValue       = template[node],
-        isString        = (typeof nodeValue == "string"),
-        hasExpressions  = isString ? nodeValue.indexOf("{{") != -1 : false,
-        expression      = hasExpressions ? nodeValue.split("{{")[1].split("}}")[0] : null,
-        expressionValue = expression ? scope.execute(expression) : null,
-        replace         = expressionValue ? nodeValue.replace("{{"+expression+"}}", expressionValue) : nodeValue,
-        isSubtree       = (typeof nodeValue == "object");
+var link = function(scope, node, nodeValue){
+      var isString        = (typeof nodeValue == "string"),
+          isSubtree       = (typeof nodeValue == "object"),
+          hasExpressions  = isString ? nodeValue.indexOf("{{") != -1 : false,
+          expression      = hasExpressions ? nodeValue.split("{{")[1].split("}}")[0] : null,
+          expressionValue = expression ? scope.execute(expression) : null,
+          parsed          = expressionValue ? nodeValue.replace("{{"+expression+"}}", expressionValue) : nodeValue;
 
-    result[node] = isSubtree ? parse(scope, nodeValue) : replace;
-  }
-
-  return result;
-};
+      return parsed = isSubtree ? parse(scope, nodeValue) : parsed;
+    },
+    parse = function (scope, template) {
+      var result = {};
+      for (var node in template) {
+        result[node] = link(scope, node,  template[node]);
+      }
+      return result;
+    };
 
 module.exports = {
   parse : function(scope, template){
