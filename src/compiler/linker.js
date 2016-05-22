062d1166e059da569198f8726e37fff1949a2f2e
nishant <nishant.singh87@gmail.com>
2016-05-22 12:42:38 +0530

    adding directives.

--- src/compiler/linker.js
+++ src/compiler/linker.js
@@ -1,24 +1,6 @@
-var allDirectives = {
-      "@repeat": {
-        scope: true,
-        link: function(scope, element, param){
-          var varName   =  param.split("in")[0].trim(),
-              listName  = param.split("in")[1].trim(),
-              list      = scope.execute(listName),
-              parsed    = [];
+var directives = require("./directives");
 
-          for(var index = 0;index < list.length; index ++){
-            var params = {};
-            params[varName]   = list[index];
-            params["$index"]  = index+"";
-            var newScope      = scope.createChild(params);
-            parsed[index]     = require("./compiler").compile(newScope, element)
-          }
-
-          return parsed;
-        }
-      }
-    },
+var allDirectives = directives.all,
     directivesOn = function (element) {
     var directives =[];
     for(var child in element){
