5a3fbe9660220daf474e75718eb04977ef267ff1
nishant <nishant.singh87@gmail.com>
2016-05-22 16:29:04 +0530

    disable repeat directive.

--- src/compiler/directives.js
+++ src/compiler/directives.js
@@ -1,23 +1,23 @@
 var all = {
-  "@repeat": {
-    scope: true,
-    link: function(scope, element, param){
-      var varName   =  param.split("in")[0].trim(),
-          listName  = param.split("in")[1].trim(),
-          list      = scope.execute(listName),
-          parsed    = [];
-
-      for(var index = 0;index < list.length; index ++){
-        var params = {};
-        params[varName]   = list[index];
-        params["$index"]  = index+"";
-        var newScope      = scope.createChild(params);
-        parsed[index]     = require("./compiler").compile(newScope, element)
-      }
-
-      return parsed;
-    }
-  }
+  //"@repeat": {
+  //  scope: true,
+  //  link: function(scope, element, param){
+  //    var varName   =  param.split("in")[0].trim(),
+  //        listName  = param.split("in")[1].trim(),
+  //        list      = scope.execute(listName),
+  //        parsed    = [];
+  //
+  //    for(var index = 0;index < list.length; index ++){
+  //      var params = {};
+  //      params[varName]   = list[index];
+  //      params["$index"]  = index+"";
+  //      var newScope      = scope.createChild(params);
+  //      parsed[index]     = require("./compiler").compile(newScope, element)
+  //    }
+  //
+  //    return parsed;
+  //  }
+  //}
 };
 
 module.exports = {
