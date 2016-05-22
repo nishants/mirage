680ab80f2df525d466c082e09aa9c656f6c04330
nishant <nishant.singh87@gmail.com>
2016-05-22 11:56:39 +0530

    inlinde repeater in demo.js

--- demo.js
+++ demo.js
@@ -4,7 +4,7 @@ mirage.post("/create").sendFile("../sample/create.json");
 mirage.get("/get/:id").sendFile("../sample/request-path-param.json");
 mirage.get("/params").sendFile("../sample/request-url-param.json");
 mirage.get("/hello").sendFile("../sample/hello.json");
-mirage.post("/repeater").sendFile("../sample/repeater.json");
+mirage.post("/repeater").sendFile("../sample/repeater-inline.json");
 
 mirage.app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
