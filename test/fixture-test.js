95d1f0468f15711f3ab1311ffb7b91e3fc879142
nishant <nishant.singh87@gmail.com>
2016-05-31 16:59:19 +0530

    fixture for samples.

--- test/fixture-test.js
+++ test/fixture-test.js
@@ -1,10 +1,19 @@
 var expect  = require('chai').expect,
-    fixture = require("./support/fixture");
+    fixture = require("./support/fixture"),
+    fs      = require("fs");
 
 describe('Mirage', function() {
   it('should return a simple response', function (done) {
     var file = fixture.file("hello");
     expect(file.body.message).to.equal("hello");
+    done();
+  });
+
+  it('should read a sample', function (done) {
+    var sample = fixture.sample("controller");
+    expect(sample.requestBody().name).to.equal("You");
+    expect(sample.responseBody().message).to.equal("Controllified !");
+    expect(sample.templatePath()).to.equal("sample/controller/template.json");
     done();
   });
 });
--- test/support/fixture.js
+++ test/support/fixture.js
@@ -1,7 +1,23 @@
 var fs = require("fs");
 
+var pathFor = function(dir, file){
+  return "sample/<dir>/<file>"
+      .replace("<dir>", dir)
+      .replace("<file>", file);
+}, read = function(path){
+  return fs.readFileSync(path) ;
+};
+
 module.exports = {
   file: function (path) {
     return JSON.parse(fs.readFileSync("sample/<path>.json".replace("<path>", path)));
+  },
+  sample: function (name) {
+    return {
+      templatePath: function(){return pathFor(name, "template.json");},
+      requestBody : function(){return JSON.parse(read(pathFor(name, "request.json")));},
+      responseBody: function(){return JSON.parse(read(pathFor(name, "response.json")));},
+      script      : function(){return read(pathFor(name, "script.js"));}
+    };
   }
 }
